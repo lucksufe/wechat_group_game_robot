e3e59a5c71fb843d6f484f5d59b74f84b32ab6a5
sunfei <459397707@qq.com>
2018-10-23 10:03:29 +0800

    update

--- robot.py
+++ robot.py
@@ -1,20 +1,18 @@
-# coding=utf-8
 from wxpy import *
 from fake_robot import *
 import quiz
-# import sys
-# reload(sys)
-# sys.setdefualtencoding('utf8')
+import sys
+
 
 USER_NAME = ''
-USER_NAME_SELF = '孙斐'
+USER_NAME_SELF = ''
 MSG_OUTPUT = []
 bot = Bot()
 bot.enable_puid()
 fake_bot = FakeRobot()
 # my_group = bot.groups().search('交流群')[0]
 # my_friend = bot.friends().search('')[0]
-# tuling = Tuling(api_key='5c2d164575d249a0afc0c4e8da744132')
+tuling = Tuling(api_key='5c2d164575d249a0afc0c4e8da744132')
 # print(my_friend)
 # my_friend.send('测试一下')
 
@@ -39,28 +37,30 @@ fake_bot = FakeRobot()
 #         # return "received:{0}({1})".format(msg.text, msg.type)
 
 # @bot.register(bot.self, except_self=False)
-@bot.register()
+@bot.register(bot.self, except_self=False)
 @fake_bot.transfer
 def message_self(msg):
     # print(dir(msg.sender))
     # print(msg.sender.user_name, msg.sender.wxid)
     # if USER_NAME_SELF in msg.sender.name:
     #     print('self')
-    if "功能展示" == msg.text:
+    if "display" == msg.text:
         return ""
-    # if "@" in msg.text:
-    #     tuling.do_reply(msg)
-    elif "我要答题" == msg.text:
+        #"我要答题" == msg.text or 
+    elif "quiz" == msg.text:
         quiz_start(msg.sender)
+    else:
+        tuling.do_reply(msg)
 
 
 def quiz_start(msg_sender):
     if fake_bot.register_activity(msg_sender.puid,'quiz'):
-        msg_sender.send("开始答题")
+        #msg_sender.send("开始答题")
+        msg_sender.send("start")
         quiz.Quiz(fake_bot, msg_sender)
     else:
-        msg_sender.send("当前有其他活动进行中")
-
+        #msg_sender.send("当前有其他活动进行中")
+        msg_sender.send("...")
 
 bot.join()
 
